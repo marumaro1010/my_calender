75a6da0e3e385719a0c1bdd1b7bc895940bb5c63
zoe <zoefeng@palmbox.com.tw>
2020-06-24 11:59:54 +0800

    版面調整及DB class製作

--- calender.php
+++ calender.php
@@ -14,54 +14,55 @@ $end_date = date('t',strtotime($year.'-'.$month));
 //=== 取得參數 ed===
 ?>
 <div class="container">
-  <h2 align="center">我的行事曆</h2>
-  <button type="button" class="btn btn-primary">新增事項</button>
-  <table class="table table-bordered">
-    <tr>
-      <td colspan=7 align=center >
-        <a href="calender.php?year=<?php echo $month-1 == 0 ? $year-1 : $year;?>&month=<?php echo $month-1==0 ? 12 : $month-1;?>" style="text-decoration:none;">◄</a>
-        <?php echo $year."-".$month; ?>
-        <a href="calender.php?year=<?php echo $month+1==13 ? $year+1 : $year;?>&month=<?php echo $month+1==13 ? 1 : $month+1; ?>" style="text-decoration:none;">►</a>
-        </td>
-    </tr>
-    <tr>
-        <th><font>日</font></th><th>一</th><th>二</th><th>三</th><th>四</th><th>五</th><th>六</th>
-    </tr>
-  </div>
-  <?php
-  $start = date('w',strtotime($start_date));
-  $end = $end_date;
-  $cal_text = '';
-  for($i=0;$i<42;$i++)
-  { 
-    if($i >= $start && $i <= ($end + ($start-1)))
-    {
-      $cal_text = ($i - ($start-1));
-    }
-    else
-    {
+  <h2 align="center">My Event</h2>
+  <h3 align="center"><?php echo $year."-".$month; ?></h3>
+  <button type="button" class="btn btn-primary" style="margin:10px" data-toggle="modal" data-target="#ooo">Add event</button>
+  <!-- <a href="calender.php?year=<?php echo $month-1 == 0 ? $year-1 : $year;?>&month=<?php echo $month-1==0 ? 12 : $month-1;?>" style="text-decoration:none;">◄</a>
+  <a href="calender.php?year=<?php echo $month+1==13 ? $year+1 : $year;?>&month=<?php echo $month+1==13 ? 1 : $month+1; ?>" style="text-decoration:none;">►</a> -->
+  <table class="table">
+      <tr class="title_bar" >
+          <th>Sun</th><th>Mon</th><th>The</th><th>Wed</th><th>Thu</th><th>Fri</th><th>Sat</th>
+      </tr>
+      <?php
+      $start = date('w',strtotime($start_date));
+      $end = $end_date;
       $cal_text = '';
-    }
+      for($i=0;$i<42;$i++)
+      { 
+        if($i >= $start && $i <= ($end + ($start-1)))
+        {
+          $cal_text = ($i - ($start-1));
+        }
+        else
+        {
+          $cal_text = '';
+        }
 
-    if($i % 7 == 0)
-    {
+        if($i % 7 == 0)
+        {
+          ?>
+          <tr>
+          <?php
+        }
+        ?>
+        <td class="slip">
+          <div>
+            <?php echo $cal_text;?>
+          </div>
+          <!-- <div class="event_slip">as2</div> -->
+        </td> 
+        <?php
+        if($i % 7 == 6)
+        {
+          ?>
+          <tr>
+          <?php
+        }
+        unset($text);
+      }
       ?>
-      <tr>
-      <?php
-    }
-    ?>
-    <td><?php echo $cal_text;?></td> 
-    <?php
-    if($i % 7 == 6)
-    {
-      ?>
-      <tr>
-      <?php
-    }
-    unset($text);
-  }
-?>
-</table>
+    </table>
+</div>
 <?php
 require_once('foot.php');
 ?>
--- head.php
+++ head.php
@@ -2,8 +2,10 @@
     <head>
         <meta charset="utf-8">
         <title>我的行事曆</title>
+        <link rel="stylesheet" href="public/css/reset.css" type="text/css" />
         <link rel="stylesheet" href="public/css/style.css" type="text/css" />
         <script src="public/js/calender.js"></script>
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto+Condensed">
         <!-- <script src="asset/js/calender.js"></script> -->
     </head>
     <body>
